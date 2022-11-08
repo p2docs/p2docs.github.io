bef2b6867c5aadd18c2fd5a47953debd3774da11
Ada Gottensträter <nunuu@gmx.net>
2022-11-08 16:31:22 +0100

    Generate sitemap.txt

---
+++ plugin/sitemap-txt.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+$finalize_hooks << lambda do
+    root_url = CONFIG[:sitemap_root_url]
+    if root_url.nil? || root_url.empty?
+        puts "Skipping sitemap.txt generation, missing sitemap_root_url"
+        return
+    end
+    File.write("out/sitemap.txt",PAGES.reject{|pg|pg.props['sitemap_exclude']}.map{|pg|root_url+pg.href_path}.join(?\n))
+end
